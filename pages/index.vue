ea1a4a7a5e29200db0be8c3c5ea25ae0169493d8
unknown <ibraumodnok@gmail.com>
2025-01-04 23:23:32 +0000

    api relation

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Hello
+        {{  data }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    const data = ref(process.env.BASE_URL)
+    const { $axios } = useNuxtApp()
+
+    // onMounted(async () => {
+    //     data.value = await $axios.get('/games')
+    // });
+</script>
+
+<style scoped>
+    
+</style>
--- plugins/axios.js
+++ plugins/axios.js
@@ -4,6 +4,21 @@ export default defineNuxtPlugin((nuxtApp) => {
   const axiosInstance = axios.create({
     baseURL: nuxtApp.$config.public.BASE_URL,
   });
+  axiosInstance.interceptors.request.use((config) => {
+    const defaultQueryParams = {
+      key: process.env.API_KEY || '',
+      lang: 'fr',
+    };
+
+    config.params = {
+      ...config.params,
+      ...defaultQueryParams,
+    };
+
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
 
   return {
     provide: {
